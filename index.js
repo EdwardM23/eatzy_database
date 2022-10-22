9cf85ecc02371d52d14c770622e4076a1092217a
Christopher Edward <scrith.edward23@gmail.com>
2022-10-23 00:23:50 +0700

    test routes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 import RestaurantRoute from "./routes/RestaurantRoute.js";
+const product = require("./routes/Test.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 app.use(RestaurantRoute);
+app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
 
