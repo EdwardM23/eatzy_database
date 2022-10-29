441f96a2a83329d9621bb9c4734748edb481008e
Christopher Edward <scrith.edward23@gmail.com>
2022-10-29 11:39:50 +0700

    fix host name

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 var db_name = "heroku_ea7df0ec128262c";
 var username = "b9592fc9f7570e";
 var password = "325757f5";
-var host_name = "http://us-cdbr-east-06.cleardb.net";
+var host_name = "us-cdbr-east-06.cleardb.net";
 
 const database = new Sequelize(db_name, username, password, {
   host: host_name,
