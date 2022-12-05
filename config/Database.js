86b87ec98ccb09fdbeeee11018413e829c738691
Christopher Edward <scrith.edward23@gmail.com>
2022-12-05 22:41:20 +0700

    fix db connection

--- config/Database.js
+++ config/Database.js
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
 
-// var db_name = "heroku_cf5c5ad343017c2";
-// var username = "b45d8558fabf0b";
-// var password = "0e87aeac";
-// var host_name = "us-cdbr-east-06.cleardb.net";
+var db_name = "heroku_cf5c5ad343017c2";
+var username = "b45d8558fabf0b";
+var password = "0e87aeac";
+var host_name = "us-cdbr-east-06.cleardb.net";
 
-var db_name = "eatzy_db";
-var username = "root";
-var password = "";
-var host_name = "";
+// var db_name = "eatzy_db";
+// var username = "root";
+// var password = "";
+// var host_name = "";
 
 const database = new Sequelize(db_name, username, password, {
   host: host_name,
