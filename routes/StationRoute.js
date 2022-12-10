72c741b415c54a8c589721a0642372717cd2994f
Christopher Edward <scrith.edward23@gmail.com>
2022-12-10 22:50:50 +0700

    add getStationById

--- routes/StationRoute.js
+++ routes/StationRoute.js
@@ -5,10 +5,12 @@ import {
   editStation,
   getAllStation,
   getNearestStation,
+  getStationById,
 } from "../controllers/StationController.js";
 
 const router = express.Router();
 router.post("/station", addStation);
+router.post("/station/:id", getStationById);
 router.post("/station/nearest", getNearestStation);
 router.patch("/station/:id", editStation);
 router.delete("/station/:id", deleteStation);
